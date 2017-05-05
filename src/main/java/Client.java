318a7b7725974b0bf4bef3ab61ac4884ed514ee8
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-05 11:53:10 +0300

    add class constructor to Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,4 +3,8 @@ import java.util.List;
 
 public class Client{
 
+  public Client(String name){
+    
+  }
+
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 
 public class ClientTest{
   public void Client_instantiateCorrectly_true(){
-    Client myClient = new Client();
+    Client myClient = new Client("Michael King");
     assertEquals(true, myClient instanceof Client);
   }
 

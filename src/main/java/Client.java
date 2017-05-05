44a4b8dfdd78f9ab1214648a41211859a33d605f
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-05 12:00:36 +0300

    initialise mName and add getName function to Client.java; comment code

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Client{
-
+  private String mName;
+  // class constructor
   public Client(String name){
-    
+    mName = name;
+  }
+
+  public String getName(){
+    return mName;
   }
 
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,15 +1,17 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
+// class constructor
 public class ClientTest{
+  // confirm we can successfully instantiate Task objects
   public void Client_instantiateCorrectly_true(){
     Client myClient = new Client("Michael King");
     assertEquals(true, myClient instanceof Client);
   }
-
+  // make sure we can assign each client a name and then retrieve it
   public void Client_instantiatesWithName_String(){
     Client myClient = new Client("Michael King");
-    assertEquals("Michael King", myClient.getDescription());
+    assertEquals("Michael King", myClient.getName());
   }
 
 }

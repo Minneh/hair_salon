890864bf9a488cc1e67632a55084b207d00cc160
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-05 11:50:57 +0300

    create first client test for correct instantiation of class

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -2,5 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ClientTest{
+  public void Client_instantiateCorrectly_true(){
+    Client myClient = new Client();
+    assertEquals(true, myClient instanceof Client);
+  }
 
 }

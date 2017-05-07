928109dd9fc9167116df60b4dee4ca79f8e0addc
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-07 15:27:30 +0300

    add new stylist route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -29,5 +29,12 @@ public class App {
       return new ModelAndView(model,layout);
     },new VelocityTemplateEngine());
 
+    get("/stylists/new", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/stylist-form.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+
   }
 }

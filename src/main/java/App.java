04d23fba422843c71e4e93ac8dcebd56ad3aced7
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-07 15:27:57 +0300

    add stylist list route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -36,5 +36,12 @@ public class App {
   }, new VelocityTemplateEngine());
 
 
+    get("/stylists", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("stylists", Category.all());
+        model.put("template", "templates/stylists.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
   }
 }

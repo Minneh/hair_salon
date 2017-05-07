5108b0ce12bb2bd3f010a6f3f6bd363c4fcd3098
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-07 11:30:37 +0300

    add test returnsAllInstancesOfClient

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 // class constructor
 public class ClientTest{
   // confirm we can successfully instantiate Task objects
-  public void Client_instantiateCorrectly_true(){
+  public void Client_instantiatesCorrectly_true(){
     Client myClient = new Client("Michael King");
     assertEquals(true, myClient instanceof Client);
   }
@@ -13,5 +13,14 @@ public class ClientTest{
     Client myClient = new Client("Michael King");
     assertEquals("Michael King", myClient.getName());
   }
+
+  @Test
+  public void all_returnsAllInstancesOfClient_true(){
+    Client firstClient = new Client("Superman", 35, "Flight", "Kryptonite");
+    Client secondClient = new Client("Antman", 32, "Shrinkage", "Aardvarks");
+    assertEquals(true, Client.all().contains(firstClient));
+    assertEquals(true, Client.all().contains(secondClient));
+    }
+
 
 }

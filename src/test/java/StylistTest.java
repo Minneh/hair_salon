40cda024a17025f640f8ac6f1d8c87aef9f1fea6
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-07 13:15:49 +0300

    add test category_instantiatesCorrectly_true to StylistTest

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -2,3 +2,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
 import java.util.Arrays;
+
+public class StylistTest{
+  @Test
+  public void category_instantiatesCorrectly_true() {
+    Category testCategory = new Category("Home");
+    assertEquals(true, testCategory instanceof Category);
+  }
+}

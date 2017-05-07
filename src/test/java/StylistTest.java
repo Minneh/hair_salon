7a743d3e89b2472277f2f0a9d29ce49b4c7bb684
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-07 13:20:11 +0300

    edit tests for Stylist

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 
 public class StylistTest{
   @Test
-  public void category_instantiatesCorrectly_true() {
-    Category testCategory = new Category("Home");
-    assertEquals(true, testCategory instanceof Category);
+  public void stylist_instantiatesCorrectly_true() {
+    Stylist testStylist = new Stylist("Jenna Marbles");
+    assertEquals(true, testStylist instanceof Stylist);
   }
 
-  
+  @Test
+  public void getName_stylistInstantiatesWithName_Home() {
+    Stylist testStylist = new Stylist("Jenna Marbles");
+    assertEquals("Jenna Marbles", testStylist.getName());
+  }
 }

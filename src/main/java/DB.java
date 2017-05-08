d87d115c4bd565bb9766c7cec2d3002a67fc394b
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-08 08:34:16 +0300

    change DB connection to support heroku hosting

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,6 @@
 import org.sql2o.*;
 
 public class DB{
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", null, null);
-  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-243-252-91.compute-1.amazonaws.com:5432/d3bgi8iu7eklb6", "qybuoagfaypzgi", "06845382a955ee23e2770737ed23c686bdc7838aec517f9336f713d8d77f3fb7");
+  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", null, null);
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-243-252-91.compute-1.amazonaws.com:5432/d3bgi8iu7eklb6", "qybuoagfaypzgi", "06845382a955ee23e2770737ed23c686bdc7838aec517f9336f713d8d77f3fb7");
 }
